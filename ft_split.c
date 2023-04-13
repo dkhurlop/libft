e8d53d9da687af5948567f33cf1be067166c737f
dkhurlop <129850727+dkhurlop@users.noreply.github.com>
2023-04-13 15:58:41 +0200

    Create ft_split.c

---
+++ ft_split.c
@@ -0,0 +1,81 @@
+#include "libft.h"
+
+static int	ft_word_counter(char const *s, char c)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (s[i])
+	{
+		while (s[i] && s[i] == c)
+			i++;
+		if (s[i])
+			count++;
+		while (s[i] && s[i] != c)
+		i++;
+	}
+	return (count);
+}
+
+static int	ft_word_len(char const *s, char c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] && s[i] != c)
+		i++;
+	return (i);
+}
+
+static char	**ft_free(char **tab, int i)
+{
+	while (i >= 0)
+	{
+		free (tab[i]);
+		i--;
+	}
+	free(tab);
+	return (NULL);
+}
+
+static char	**ft_fill_tab(char const *s, char c, char **tab)
+{
+	int	i;
+	int	j;
+	int	k;
+
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		while (s[i] && s[i] == c)
+			i++;
+		if (s[i])
+		{
+			k = 0;
+			tab[j] = malloc(sizeof(char) * (ft_word_len(s + i, c) + 1));
+			if (!tab[j])
+				return (ft_free(tab, j));
+			while (s[i] && s[i] != c)
+				tab[j][k++] = s[i++];
+				tab[j++][k] = '\0';
+		}
+	}
+	tab[j] = NULL;
+	return (tab);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**tab;
+
+	if (!s)
+		return (NULL);
+	tab = malloc(sizeof(char *) * (ft_word_counter(s, c) + 1));
+	if (tab == NULL)
+		return (NULL);
+	tab = ft_fill_tab(s, c, tab);
+	return (tab);
+}
