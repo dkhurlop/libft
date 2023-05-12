3c1fb5d0075738ce2fb729608281ade940229479
dkhurlop <129850727+dkhurlop@users.noreply.github.com>
2023-05-12 10:46:21 +0200

    ft_strmapi.c

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -31,3 +31,17 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	str[i] = '\0';
 	return (str);
 }
+
+/*
+Parámetros s: La string que iterar.
+f: La función a aplicar sobre cada carácter.
+Valor devuelto La string creada tras el correcto uso de ’f’ sobre
+cada carácter.
+NULL si falla la reserva de memoria.
+Funciones autorizadas
+malloc
+Descripción A cada carácter de la string ’s’, aplica la
+función ’f’ dando como parámetros el índice de cada
+carácter dentro de ’s’ y el propio carácter. Genera
+una nueva string con el resultado del uso sucesivo
+de ’f’
